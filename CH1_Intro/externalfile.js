3d12438f5a9ba922a7949c30829ea772968084fa
abdelmeenam <abdelmeenam@gmail.com>
2021-12-04 20:48:06 +0200

    CH1 & CH2

---
+++ CH1_Intro/externalfile.js
@@ -0,0 +1,4 @@
+//this appears only in console enviroment
+console.log("data appers only in console enviroment");
+console.warn("this is warn");
+console.error("%c this is blue error" ,"color:blue");
