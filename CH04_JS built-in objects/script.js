3a8e70976ae04c6e4064415e40a3a0d5745f5873
abdelmeenam <abdelmeenam@gmail.com>
2021-12-07 21:57:12 +0200

    string methods

---
+++ CH04_JS built-in objects/script.js
@@ -0,0 +1,34 @@
+//Literal creation
+var myStr ="hello world from java script"
+
+//constructor
+var newStr = new String("hello world");
+
+var str = String("hello world")
+
+console.log(typeof myStr)
+console.log(typeof newStr)
+console.log(typeof str)
+
+
+//Length
+console.log( myStr.length)                       //11
+
+//index by num
+console.log( myStr.charAt(10))                   //d
+console.log( myStr.charAt(myStr.length-1))       //d
+
+//index of specifi char
+console.log( myStr.indexOf("o"))                  //4
+console.log( myStr.indexOf("ld"))                 //9
+console.log( myStr.lastIndexOf("l"))              //9
+
+//cut part of string
+console.log(myStr.substring(6,11))                 //world
+
+//convert string into array
+console.log(myStr.split(" "))
+console.log(myStr.split(""))
+
+
+
